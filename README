955a0e13244baf198a8095299f2a8f556a483f32
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2011-12-17 15:17:44 +0900

    ためしに公開

---
+++ README
@@ -0,0 +1,21 @@
+# Remove Exif Plug-in for MovableType
+* Author:: Yuichi Takeuchi <uzuki05@takeyu-web.com>
+* Website:: http://takeyu-web.com/
+* Copyright:: Copyright 2011 Yuichi Takeuchi
+* License:: MIT-License
+
+pluginsディレクトリに配置すると、アセットのサムネイル生成の際にExif情報を削除するようになります。
+
+## 動作（するかもしれない）環境
+* MovableType/MTOS 5
+** MovableType 4 は未検証
+* Image::Magick が利用できるサーバー
+
+## しくみ
+MT標準のサムネイル生成関数をフックして、サムネイル削除コードを挿入します。
+
+フックするだけなので、サムネイル生成の挙動自体はかわりません。
+
+
+## ご注意
+* 無保証です。このプラグインを使用したことでいかなる損害が発生しても、制作者は一切責任を負いません。
---
+++ rm-exif.pl
@@ -0,0 +1,48 @@
+package MT::Plugin::RmExif;
+
+use strict;
+use warnings;
+no warnings 'redefine'; 
+
+use MT;
+use MT::Plugin;
+my $plugin = MT::Plugin->new({
+    name => 'Remove Exif',
+    version => '1.0.0',
+    description => "Delete the Exif when generating thumbnails.",
+    author_name => 'Yuichi Takeuchi',
+    author_link => 'http://takeyu-web.com/',
+});
+
+MT->add_plugin($plugin);
+
+use MT::Asset::Image;
+
+package MT::Asset::Image;
+
+my $redefined = defined &thumbnail_file_with_strip;
+
+unless ( $redefined ) {
+
+    my $thumbnail_file_without_strip = *thumbnail_file{CODE};
+
+    *thumbnail_file_with_strip = sub {
+        my ( $thumbnail, $n_w, $n_h ) = $thumbnail_file_without_strip->( @_ );
+        
+        if ( -f $thumbnail ) {
+            require Image::Magick;
+            my $thumb = Image::Magick->new();
+            $thumb->Read( $thumbnail );
+            $thumb->Profile( name=>"*", profile=>"" );
+            $thumb->[0]->Write( filename => $thumbnail )
+        }
+        
+        return ( $thumbnail, $n_w, $n_h );
+    };
+
+    *thumbnail_file = *thumbnail_file_with_strip;
+
+}
+
+1;
+
